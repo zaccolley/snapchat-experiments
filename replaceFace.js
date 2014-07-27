c9335d1e4bbba644beca1f148027116de063c74d
Zac Colley <hi@zaccolley.com>
2014-07-27 15:32:30 +0100

    Using the gm node module now, tidied up file

--- replaceFace.js
+++ replaceFace.js
@@ -1,37 +1,53 @@
 var cv = require('opencv'),
 	fs = require('fs'),
-	exec = require('child_process').exec,
+	gm = require('gm'),
 
-	bgImg = "images/pete.jpg",
+	baseImg = "images/mona.png",
 	overlayImg = "images/cat.png",
-	outputImg = "images/test.png";
+	outputImg = "images/output.jpg";
 
-cv.readImage(bgImg, function(err, im){
+cv.readImage(baseImg, function(err, im){
 
-	if(err){
-		console.log("Error: " + err);
-	}else{
+	if(err) return console.log("Error: " + err); // return any errors
 
-		im.detectObject("./haarcascade_frontalface_alt.xml", {}, function(err, faces){  
+	im.detectObject("./haarcascade_frontalface_alt.xml", {}, function(err, images){
 
-			var biggestFace = faces[0];
+		if(err) return console.log("Error: " + err); // return any errors
 
-			for (var i = 1; i < faces.length; i++){
-				var face = faces[i];
-
-				// compare to see if face is bigger or not
-				if( (face.width > biggestFace.width) && (face.height > biggestFace.height) ){
-					biggestFace = face;
-				}
-
-			}
-
-			var face = biggestFace;
-			
-			exec("gm composite -geometry +"+face.x+"+"+face.y+" -resize "+face.width+"x"+face.height+" "+overlayImg+" "+bgImg+" /output/"+outputImg);
-
+		var face = getBiggestImage(images);
+		compositeImages(baseImg, overlayImg, outputImg, face, function(err){
+			if(err) return console.dir(arguments); // return any errors
+			console.log("gm command: ", arguments[3]); // display the command generated
 		});
+
+	});
+
+});
+
+// composite image over another
+function compositeImages(baseImg, overlayImg, outputImg, overlayImgData, callback){
+
+	gm().subCommand('composite')
+		.out('-geometry', '+'+overlayImgData.x+'+'+overlayImgData.y) // position of overlayed image
+		.out('-resize', overlayImgData.width+'x'+overlayImgData.height) // resize the overlayed image
+		.out(overlayImg, baseImg)
+		.write(outputImg, callback);
+
+}
+
+// find the biggest image
+function getBiggestImage(imgs){
+	var biggestImg = imgs[0];
+
+	for (var i = 1; i < imgs.length; i++){
+		var img = imgs[i];
+
+		// compare to see if Image is bigger or not
+		if( (img.width > biggestImg.width) && (img.height > biggestImg.height) ){
+			biggestImg = img;
+		}
 
 	}
 
-});
+	return biggestImg;
+}
